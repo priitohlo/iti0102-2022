b4fdf0a974379434c5f881e951d5de29bd21f5d7
Priit Ohlo <prohlo@ttu.ee>
2022-09-04 22:54:35 +0300

    Fix EX01, attempt 2

--- EX/ex01_hello/hello.py
+++ EX/ex01_hello/hello.py
@@ -12,7 +12,7 @@ Great! Now enter a second random number: 4
 # ask for a name
 name = input("What is your name? ")
 # ask for first random number
-num1 = int(input(f"Hello {name}!, Enter a random number: "))
+num1 = int(input(f"Hello, {name}! Enter a random number: "))
 # ask for second random number
 num2 = int(input("Great! Now enter a second random number: "))
 # print out sum
