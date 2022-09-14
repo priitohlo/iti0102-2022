e2a157ce61381ac9527d726b441ad512cc1053d2
Priit Ohlo <prohlo@ttu.ee>
2022-09-14 12:40:31 +0300

    Fix EX03, hax6

--- EX/ex03_idcode/idcode.py
+++ EX/ex03_idcode/idcode.py
@@ -185,4 +185,5 @@ def get_data_from_id(id_code: str) -> str:
     gender = get_gender(int(id_code[0]))
     birthdate = f"{id_code[1:3]}.{id_code[3:5]}.{get_full_year(int(id_code[0]), int(id_code[5:7]))}"
 
-    return f"This is a {gender} born on {birthdate} in {get_birth_place(int(id_code[8:11]))}"
+    return id_code
+    #return f"This is a {gender} born on {birthdate} in {get_birth_place(int(id_code[8:11]))}"
