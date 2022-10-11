f968af098bd7aa9acd99ca30b847b141965ebd40
Priit Ohlo <prohlo@ttu.ee>
2022-10-11 17:51:11 +0300

    EX05, test 7

--- EX/ex05_hobbies/hobbies.py
+++ EX/ex05_hobbies/hobbies.py
@@ -11,15 +11,15 @@ def create_dictionary(data: str) -> dict:
     :return: dictionary where keys are people and values are lists of hobbies
     """
 
-    ret_dict = {}
+    return_dict = {}
 
     for p in data.splitlines():
         hobby = p.split(":")
-        if hobby[0] not in ret_dict.keys():
-            ret_dict[hobby[0]] = set()
-        ret_dict[hobby[0]].add(hobby[1])
+        if hobby[0] not in return_dict.keys():
+            return_dict[hobby[0]] = set()
+        return_dict[hobby[0]].add(hobby[1])
 
-    return ret_dict
+    return return_dict
 
 
 def sort_dictionary(dic: dict) -> dict:
@@ -50,28 +50,13 @@ def create_dictionary_with_hobbies(data: str) -> dict:
     :param data: given string from database
     :return: dictionary, where keys are hobbies and values are lists of people. Values are sorted alphabetically
     """
-    hobbies_dict = create_dictionary(data)
-    sorted_dict = sort_dictionary(hobbies_dict)
-    listed_dict = {}
+
     return_dict = {}
 
-    for k, v in sorted_dict.items():
-        listed_dict[k] = list(v)
+    for p in data.splitlines():
+        hobby = p.split(":")
+        if hobby[1] not in return_dict.keys():
+            return_dict[hobby[1]] = set()
+        return_dict[hobby[0]].add(hobby[0])
 
-    sorted_keys = sorted(listed_dict.keys())
-
-    for k in sorted_keys:
-        return_dict[k] = listed_dict[k]
-
-    return return_dict
-
-if __name__ == '__main__':
-    sample_data = """Jack:crafting\nPeter:hiking\nWendy:gaming\nMonica:tennis\nChris:origami\nSophie:sport\nMonica:design\nCarmen:sport\nChris:sport\nMonica:skateboarding\nCarmen:cooking\nWendy:photography\nMonica:tennis\nCooper:yoga\nWendy:sport\nCooper:movies\nMonica:theatre\nCooper:yoga\nChris:gaming\nMolly:fishing\nJack:skateboarding\nWendy:fishing\nJack:drawing\nMonica:baking\nSophie:baking\nAlfred:driving\nAlfred:shopping\nAlfred:crafting\nJack:drawing\nCarmen:shopping\nCarmen:driving\nPeter:drawing\nCarmen:shopping\nWendy:fitness\nAlfred:travel\nJack:origami\nSophie:design\nJack:pets\nCarmen:dance\nAlfred:baking\nSophie:sport\nPeter:gaming\nJack:skateboarding\nCooper:football\nAlfred:sport\nCooper:fitness\nChris:yoga\nWendy:football\nMolly:design\nJack:hiking\nMonica:pets\nCarmen:photography\nJack:baking\nPeter:driving\nChris:driving\nCarmen:driving\nPeter:theatre\nMolly:hiking\nWendy:puzzles\nJack:crafting\nPeter:photography\nCarmen:theatre\nSophie:crafting\nCarmen:cooking\nAlfred:gaming\nPeter:theatre\nCooper:hiking\nChris:football\nChris:pets\nJack:football\nMonica:skateboarding\nChris:driving\nCarmen:pets\nCooper:gaming\nChris:hiking\nJack:cooking\nPeter:fishing\nJack:gaming\nPeter:origami\nCarmen:movies\nSophie:driving\nJack:sport\nCarmen:theatre\nWendy:shopping\nCarmen:pets\nWendy:gaming\nSophie:football\nWendy:theatre\nCarmen:football\nMolly:theatre\nPeter:theatre\nMonica:flowers\nMolly:skateboarding\nPeter:driving\nSophie:travel\nMonica:photography\nCooper:cooking\nJack:fitness\nPeter:cooking\nChris:gaming"""
-    dic = create_dictionary_with_hobbies(sample_data)
-    print("shopping" in dic["Wendy"])  # -> True
-    print("fitness" in dic["Sophie"])  # -> False
-    print("gaming" in dic["Peter"])  # -> True
-    print(len(dic["Jack"]))  # ->  12
-    print(len(dic["Carmen"]))  # -> 10
-    print(len(dic["Molly"]))  # -> 5
-    print(len(dic["Sophie"]))  # -> 7
+    return sort_dictionary(return_dict)
