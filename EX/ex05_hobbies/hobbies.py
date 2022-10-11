eb3563517215f6f9d8f4282a33fa0f8dc873b835
Priit Ohlo <prohlo@ttu.ee>
2022-10-11 21:18:43 +0300

    EX05, test 34

--- EX/ex05_hobbies/hobbies.py
+++ EX/ex05_hobbies/hobbies.py
@@ -256,7 +256,7 @@ def find_two_people_with_most_common_hobbies(data: str) -> tuple | None:
     return highest_ratio_people
 
 
-if __name__ == '__main__':
+#if __name__ == '__main__':
     # sample_data = """Jack:crafting\nPeter:hiking\nWendy:gaming\nMonica:tennis\nChris:origami\nSophie:sport\nMonica:design\nCarmen:sport\nChris:sport\nMonica:skateboarding\nCarmen:cooking\nWendy:photography\nMonica:tennis\nCooper:yoga\nWendy:sport\nCooper:movies\nMonica:theatre\nCooper:yoga\nChris:gaming\nMolly:fishing\nJack:skateboarding\nWendy:fishing\nJack:drawing\nMonica:baking\nSophie:baking\nAlfred:driving\nAlfred:shopping\nAlfred:crafting\nJack:drawing\nCarmen:shopping\nCarmen:driving\nPeter:drawing\nCarmen:shopping\nWendy:fitness\nAlfred:travel\nJack:origami\nSophie:design\nJack:pets\nCarmen:dance\nAlfred:baking\nSophie:sport\nPeter:gaming\nJack:skateboarding\nCooper:football\nAlfred:sport\nCooper:fitness\nChris:yoga\nWendy:football\nMolly:design\nJack:hiking\nMonica:pets\nCarmen:photography\nJack:baking\nPeter:driving\nChris:driving\nCarmen:driving\nPeter:theatre\nMolly:hiking\nWendy:puzzles\nJack:crafting\nPeter:photography\nCarmen:theatre\nSophie:crafting\nCarmen:cooking\nAlfred:gaming\nPeter:theatre\nCooper:hiking\nChris:football\nChris:pets\nJack:football\nMonica:skateboarding\nChris:driving\nCarmen:pets\nCooper:gaming\nChris:hiking\nJack:cooking\nPeter:fishing\nJack:gaming\nPeter:origami\nCarmen:movies\nSophie:driving\nJack:sport\nCarmen:theatre\nWendy:shopping\nCarmen:pets\nWendy:gaming\nSophie:football\nWendy:theatre\nCarmen:football\nMolly:theatre\nPeter:theatre\nMonica:flowers\nMolly:skateboarding\nPeter:driving\nSophie:travel\nMonica:photography\nCooper:cooking\nJack:fitness\nPeter:cooking\nChris:gaming"""
     #
     # sort_result = sort_names_and_hobbies(sample_data)
@@ -281,5 +281,5 @@ if __name__ == '__main__':
     # print(find_two_people_with_most_common_hobbies(sample_data))  # ('Mary', 'Nora')
 
     #sample_data = """name0:hobby5\nname10:hobby5\nname1:hobby10\nname1:hobby4\nname9:hobby0\nname0:hobby5\nname4:hobby2\nname6:hobby9\nname6:hobby9\nname9:hobby1"""
-    sample_data = """a:b\nc:d"""
-    print(find_two_people_with_most_common_hobbies(sample_data))  # ('Mary', 'Nora')
+#    sample_data = """name2:hobby0\nname2:hobby6\nname1:hobby3\nname2:hobby7\nname3:hobby7\nname6:hobby7\nname4:hobby11\nname3:hobby0\nname6:hobby0\nname0:hobby3\nname2:hobby10"""
+#    print(find_two_people_with_most_common_hobbies(sample_data))  # ('Mary', 'Nora')
