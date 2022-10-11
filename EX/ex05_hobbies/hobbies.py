3a6da6f0f0a014e73e7980137689364a58989f85
Priit Ohlo <prohlo@ttu.ee>
2022-10-11 16:46:50 +0300

    EX05, test 1

---
+++ EX/ex05_hobbies/hobbies.py
@@ -0,0 +1,55 @@
+"""EX05 - Hobbies."""
+
+def create_dictionary(data: str) -> dict:
+    """
+    Create dictionary about people and their hobbies ie. {name1: [hobby1, hobby2, ...], name2: [...]}.
+
+    There should be no duplicate hobbies on 1 person.
+
+    :param data: given string from database
+    :return: dictionary where keys are people and values are lists of hobbies
+    """
+
+    ret_dict = {}
+
+    for p in str.splitlines():
+        hobby = p.split(":")
+        if p not in ret_dict:
+            ret_dict[hobby[0]] = set(hobby[1])
+        else:
+            ret_dict[hobby[0]].add(hobby[1])
+
+    return ret_dict
+
+
+def sort_dictionary(dic: dict) -> dict:
+    """
+    Sort dictionary values alphabetically.
+
+    The order of keys is not important.
+
+    sort_dictionary({"b":[], "a":[], "c": []})  => {"b":[], "a":[], "c": []}
+    sort_dictionary({"": ["a", "f", "d"]})  => {"": ["a", "d", "f"]}
+    sort_dictionary({"b":["d", "a"], "a":["c", "f"]})  => {"b":["a", "d"], "a":["c", "f"]}
+    sort_dictionary({"Jack": ["swimming", "hiking"], "Charlie": ["games", "yoga"]})
+        => {"Jack": ["hiking", "swimming"], "Charlie": ["games", "yoga"]}
+
+    :param dic: dictionary to sort
+    :return: sorted dictionary
+    """
+
+
+    return {}
+
+
+if __name__ == '__main__':
+    sample_data = """Jack:crafting\nPeter:hiking\nWendy:gaming\nMonica:tennis\nChris:origami\nSophie:sport\nMonica:design\nCarmen:sport\nChris:sport\nMonica:skateboarding\nCarmen:cooking\nWendy:photography\nMonica:tennis\nCooper:yoga\nWendy:sport\nCooper:movies\nMonica:theatre\nCooper:yoga\nChris:gaming\nMolly:fishing\nJack:skateboarding\nWendy:fishing\nJack:drawing\nMonica:baking\nSophie:baking\nAlfred:driving\nAlfred:shopping\nAlfred:crafting\nJack:drawing\nCarmen:shopping\nCarmen:driving\nPeter:drawing\nCarmen:shopping\nWendy:fitness\nAlfred:travel\nJack:origami\nSophie:design\nJack:pets\nCarmen:dance\nAlfred:baking\nSophie:sport\nPeter:gaming\nJack:skateboarding\nCooper:football\nAlfred:sport\nCooper:fitness\nChris:yoga\nWendy:football\nMolly:design\nJack:hiking\nMonica:pets\nCarmen:photography\nJack:baking\nPeter:driving\nChris:driving\nCarmen:driving\nPeter:theatre\nMolly:hiking\nWendy:puzzles\nJack:crafting\nPeter:photography\nCarmen:theatre\nSophie:crafting\nCarmen:cooking\nAlfred:gaming\nPeter:theatre\nCooper:hiking\nChris:football\nChris:pets\nJack:football\nMonica:skateboarding\nChris:driving\nCarmen:pets\nCooper:gaming\nChris:hiking\nJack:cooking\nPeter:fishing\nJack:gaming\nPeter:origami\nCarmen:movies\nSophie:driving\nJack:sport\nCarmen:theatre\nWendy:shopping\nCarmen:pets\nWendy:gaming\nSophie:football\nWendy:theatre\nCarmen:football\nMolly:theatre\nPeter:theatre\nMonica:flowers\nMolly:skateboarding\nPeter:driving\nSophie:travel\nMonica:photography\nCooper:cooking\nJack:fitness\nPeter:cooking\nChris:gaming"""
+    dic = create_dictionary(sample_data)
+    print("shopping" in dic["Wendy"])  # -> True
+    print("fitness" in dic["Sophie"])  # -> False
+    print("gaming" in dic["Peter"])  # -> True
+    print(len(dic["Jack"]))  # ->  12
+    print(len(dic["Carmen"]))  # -> 10
+    print(len(dic["Molly"]))  # -> 5
+    print(len(dic["Sophie"]))  # -> 7
+
