3e76c9a9244ea56ba8c39eacfd556b59345774b2
Priit Ohlo <prohlo@ttu.ee>
2022-10-18 02:19:22 +0300

    EX05, test 40

--- EX/ex05_hobbies/hobbies.py
+++ EX/ex05_hobbies/hobbies.py
@@ -243,7 +243,7 @@ def find_two_people_with_most_common_hobbies(data: str) -> tuple | None | set:
                 ratio = len(set(v).intersection(w)) / len(set(v).symmetric_difference(w))
             except ZeroDivisionError:
                 ratio = len(set(v).intersection(w))
-                if ratio > highest_ratio:
+                if ratio >= highest_ratio:
                     highest_ratio = len(set(v).intersection(w))
                     highest_ratio_people = (m, k)
                 zero_found = True
@@ -255,8 +255,8 @@ def find_two_people_with_most_common_hobbies(data: str) -> tuple | None | set:
     return set(highest_ratio_people)
 
 
-# if __name__ == '__main__':
-#
-#     sample_data = """name3:hobby9\nname7:hobby1\nname10:hobby1\nname2:hobby2\nname9:hobby9\nname2:hobby2\nname14:hobby10\nname10:hobby7\nname7:hobby7\nname5:hobby1\nname6:hobby4\nname7:hobby5\nname12:hobby4\nname7:hobby9\nname10:hobby5\nname1:hobby8\nname0:hobby5,"""
-#     data = find_two_people_with_most_common_hobbies(sample_data)
-#     print(data)
+if __name__ == '__main__':
+
+    sample_data = """name4:hobby7\nname8:hobby4\nname5:hobby11\nname1:hobby0\nname6:hobby9\nname5:hobby9\nname3:hobby10\nname2:hobby0\nname1:hobby0\nname4:hobby2"""
+    data = find_two_people_with_most_common_hobbies(sample_data)
+    print(data)
