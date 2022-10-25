bfbf9fde4e4c8f6441abf40f69fc43876ff8e463
Priit Ohlo <prohlo@ttu.ee>
2022-10-25 22:13:51 +0300

    EX07, test dunno 20

--- EX/ex07_files/file_handling.py
+++ EX/ex07_files/file_handling.py
@@ -397,7 +397,8 @@ def read_csv_file_into_list_of_dicts_using_datatypes(filename: str) -> list:
 
     for e in input_dicts:
         for k, v in e.items():
-            if r.match(v):
+            if r.match(v) and (not list(itertools.filterfalse(r.match, [e[k] for e in input_dicts]))
+                                 or list(filter(lambda x: x == '-', [e[k] for e in input_dicts]))):
                 dates.append(k)
 
     for i, e in enumerate(input_dicts):
@@ -406,10 +407,8 @@ def read_csv_file_into_list_of_dicts_using_datatypes(filename: str) -> list:
             if v.isnumeric() and "".join(
                     list(itertools.filterfalse(lambda x: x == '-', [e[k] for e in input_dicts]))).isnumeric():
                 output_dicts[i][k] = int(v)
-            elif r.match(v):
+            elif r.match(v) and k in dates:
                 output_dicts[i][k] = datetime.datetime.strptime(v, '%d.%m.%Y').date()
-            elif k in dates:
-                output_dicts[i][k] = None
             elif v == "-":
                 output_dicts[i][k] = None
             else:
