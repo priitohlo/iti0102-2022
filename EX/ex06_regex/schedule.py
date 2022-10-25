001193dafe57f0e0daf614ee7880b15fbcd3c011
Priit Ohlo <prohlo@ttu.ee>
2022-10-25 15:14:58 +0300

    EX06, test 42

--- EX/ex06_regex/schedule.py
+++ EX/ex06_regex/schedule.py
@@ -59,7 +59,7 @@ def create_schedule_file(input_filename: str, output_filename: str) -> None:
 def create_schedule_string(input_string: str) -> str:
     """Create schedule string from the given input string."""
     times_dict = dict()
-    pattern = "(\d{1,2}:\d{1,2}) ([a-zA-Z(, )?]*)"
+    pattern = "(\d{1,2}:\d{1,2}) (([a-zA-Z]+)(, [a-zA-Z]+))"
 
     match = re.findall(pattern, input_string)
     for m in match:
@@ -74,5 +74,5 @@ def create_schedule_string(input_string: str) -> str:
 
 
 if __name__ == '__main__':
-    print(create_schedule_string("1:02 correct, done, done"))
+    print(create_schedule_string("1:02 correct done"))
     #create_schedule_file("schedule_input.txt", "schedule_output.txt")
