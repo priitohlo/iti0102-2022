3f0135c45db2f9bdfde1ddb94baf2eca90c172f5
Priit Ohlo <prohlo@ttu.ee>
2022-10-25 06:04:15 +0300

    EX06, test 32

--- EX/ex06_regex/schedule.py
+++ EX/ex06_regex/schedule.py
@@ -3,6 +3,38 @@ from datetime import datetime
 import re
 
 
+def create_table(input_dict: dict) -> str:
+    sorted_input_dict = dict(sorted(input_dict.items(),
+                                    key=lambda x: datetime.strptime(x[0], '%I:%M %p')))
+    longest_entry_length = len(max(sorted_input_dict.values(), key=len, default=''))
+
+
+    if longest_entry_length:
+        top_bottom_border = longest_entry_length * '-' + 15 * '-' + '\n'
+        header = '|     time | entries' + (longest_entry_length - 6) * ' ' + '|\n'
+    else:
+        top_bottom_border = 20 * '-' + '\n'
+        header = '|  time | entries  |\n'
+
+    out_buffer = top_bottom_border
+    out_buffer += header
+    out_buffer += top_bottom_border
+
+    if longest_entry_length > 0:
+        for k, v in sorted_input_dict.items():
+            out_buffer += '| '
+            out_buffer += k
+            out_buffer += ' | '
+            out_buffer += v + (longest_entry_length - len(v)) * ' '
+            out_buffer += ' |\n'
+    else:
+        out_buffer += '| No entries found |\n'
+
+    out_buffer += top_bottom_border
+
+    return out_buffer
+
+
 def time_normalize(date: str):
     """Add missing 0's to the minutes and remove extra 0's from hours."""
     return datetime.strftime(datetime.strptime(date, '%H:%M'), '%I:%M %p')
@@ -25,7 +57,6 @@ def create_schedule_file(input_filename: str, output_filename: str) -> None:
 def create_schedule_string(input_string: str) -> str:
     """Create schedule string from the given input string."""
     times_dict = dict()
-    longest_entry_length = 0
     pattern = "(\d{1,2}:\d{1,2}) ([a-zA-Z]+)"
 
     match = re.findall(pattern, input_string)
@@ -34,27 +65,10 @@ def create_schedule_string(input_string: str) -> str:
             times_dict[time_normalize(m[0])] = m[1]
         except ValueError:
             continue
-        if len(m[1]) > longest_entry_length:
-            longest_entry_length = len(m[1])
 
-    times_dict = dict(sorted(times_dict.items(), key=lambda x: datetime.strptime(x[0], '%I:%M %p')))
+    table = create_table(times_dict)
 
-    top_bottom_border = longest_entry_length * '-' + 15 * '-' + '\n'
-    out_buffer = top_bottom_border
-    out_buffer += '|     time | entries' + (longest_entry_length - 6) * ' ' + '|\n'
-    out_buffer += top_bottom_border
-
-    for k, v in times_dict.items():
-        out_buffer += '| '
-        out_buffer += k
-        out_buffer += ' | '
-        out_buffer += v + (longest_entry_length - len(v)) * ' '
-        out_buffer += ' |\n'
-
-    out_buffer += top_bottom_border
-
-
-    return out_buffer
+    return table
 
 
 if __name__ == '__main__':
