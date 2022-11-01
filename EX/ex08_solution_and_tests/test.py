6202efd6256fc052bc3d984331fb505a580b3d44
Priit Ohlo <prohlo@ttu.ee>
2022-11-01 19:07:31 +0200

    EX08, test 11

--- EX/ex08_solution_and_tests/test.py
+++ EX/ex08_solution_and_tests/test.py
@@ -19,10 +19,10 @@ def test_student_study__evening_coffee_true():
     assert solution.students_study(19, True) is True
 
 def test_student_study__evening_edge_case_coffee_true():
-    assert solution.students_study(17, True) is True
+    assert solution.students_study(18, True) is True
 
 def test_student_study__evening_edge_case_coffee_false():
-    assert solution.students_study(17, False) is True
+    assert solution.students_study(18, False) is True
 
 def test_student_study__night_edge_case_coffee_true():
     assert solution.students_study(4, True) is False
