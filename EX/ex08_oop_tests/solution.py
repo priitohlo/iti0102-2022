f129cbd19aa87a3eb77bc82c9cfc8b00cb2973aa
Priit Ohlo <prohlo@ttu.ee>
2022-11-08 19:39:05 +0200

    EX

--- EX/ex08_oop_tests/solution.py
+++ EX/ex08_oop_tests/solution.py
@@ -1,11 +1,13 @@
+"""TODO: documentation."""
+
 class Factory:
     def __init__(self):
-        """TODO: documentation"""
+        """TODO: documentation."""
         self.cakes_baked = list()
         self.cakes_baked_last = list()
 
     def bake_cake(self, toppings: int, base: int) -> int:
-        """TODO: documentation"""
+        """TODO: documentation."""
         self.cakes_baked_last.clear()
         ingredients_amount = toppings if toppings == base else max(toppings, base)
         for i in (5, 2, 1):
@@ -17,26 +19,26 @@ class Factory:
         return len(self.cakes_baked_last)
 
     def get_last_cakes(self, n: int) -> list:
-        """TODO: documentation"""
+        """TODO: documentation."""
         return self.cakes_baked_last[-n:] if n != 0 else []
 
     def get_cakes_baked(self) -> list:
-        """TODO: documentation"""
+        """TODO: documentation."""
         return self.cakes_baked
 
     def __str__(self):
-        """TODO: documentation"""
+        """TODO: documentation."""
         return f"Factory with {len(self.cakes_baked)} cakes." if len(self.cakes_baked) != 1 else "Factory with 1 cake."
 
     def __repr__(self):
-        """TODO: documentation"""
+        """TODO: documentation."""
         return f"{len(self.cakes_baked_last)} cakes"
 
 
 class Cake:
 
     def __init__(self, base_amount, toppings_amount):
-        """TODO: documentation"""
+        """TODO: documentation."""
         match (base_amount, toppings_amount):
             case (1, 1):
                 self.type = "basic"
@@ -49,16 +51,16 @@ class Cake:
 
     @property
     def type(self):
-        """TODO: documentation"""
+        """TODO: documentation."""
         return self._type
 
     @type.setter
     def type(self, value):
-        """TODO: documentation"""
+        """TODO: documentation."""
         self._type = value
 
     def __repr__(self):
-        """TODO: documentation"""
+        """TODO: documentation."""
         match self.type:
             case "basic":
                 return "Cake(basic)"
@@ -68,11 +70,11 @@ class Cake:
                 return "Cake(large)"
 
     def __str__(self):
-        """TODO: documentation"""
+        """TODO: documentation."""
         return self.__repr__()
 
     def __eq__(self, other):
-        """TODO: documentation"""
+        """TODO: documentation."""
         return self.type == other.type
 
 
