7efcabb42034b3a875f97d5a583b0a0f48272b2d
Priit Ohlo <prohlo@ttu.ee>
2022-11-15 01:05:03 +0200

    EX

--- EX/ex07_files/file_handling.py
+++ EX/ex07_files/file_handling.py
@@ -554,20 +554,24 @@ def generate_people_report(person_data_directory: str, report_filename: str) -> 
 
     try:
         people_dict = dict(sorted(people_dict.items(), key=lambda x: (cmpnonedate(x[1]["name"]))))
-    except KeyError:
-        pass
-
-    try:
         people_dict = dict(
             sorted(people_dict.items(), key=lambda x: datetime.strptime(cmpnonedate(x[1]["birth"]), '%d.%m.%Y'),
                    reverse=True))
-    except KeyError:
-        pass
-
-    try:
         people_dict = dict(sorted(people_dict.items(), key=lambda x: (x[1]["age"] == -1, x[1]["age"])))
     except KeyError:
         pass
+
+    # try:
+    #     people_dict = dict(
+    #         sorted(people_dict.items(), key=lambda x: datetime.strptime(cmpnonedate(x[1]["birth"]), '%d.%m.%Y'),
+    #                reverse=True))
+    # except KeyError:
+    #     pass
+    #
+    # try:
+    #     people_dict = dict(sorted(people_dict.items(), key=lambda x: (x[1]["age"] == -1, x[1]["age"])))
+    # except KeyError:
+    #     pass
 
     with open(report_filename, 'w') as f:
         f.writelines(",".join(list(next(iter(people_dict.values())).keys())) + '\n')
