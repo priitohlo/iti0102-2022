ee08d35ea3506b993ffd3a182e2c4d9ff4854a22
Priit Ohlo <prohlo@ttu.ee>
2022-11-17 02:47:08 +0200

    EX

--- EX/ex09_recursion/inception.py
+++ EX/ex09_recursion/inception.py
@@ -76,9 +76,11 @@ def sum_squares(nested_list):
     if not nested_list:
         return 0
     else:
-        print(nested_list)
-        return sum([x ** 2 for x in nested_list if type(x) == int]) + \
-            sum_squares([x for x in nested_list if type(x) == list])
+        print([x for x in nested_list if type(x) == list])
+        return_sum = sum([x ** 2 for x in nested_list if type(x) == int])
+        for x in [x for x in nested_list if type(x) == list]:
+            return_sum += sum_squares(x)
+        return return_sum
 
 
 def count_strings(data: list, pos=None, result: dict = None) -> dict:
