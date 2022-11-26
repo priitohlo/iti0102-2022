c0a5a58083a1e35370c5a62df4d3e8827e02b574
Priit Ohlo <prohlo@ttu.ee>
2022-11-26 23:26:04 +0200

    EX

--- EX/ex10_oop/twitter.py
+++ EX/ex10_oop/twitter.py
@@ -49,7 +49,7 @@ def sort_by_popularity(tweets: list) -> list:
     :param tweets: Input list of tweets.
     :return: List of tweets by popularity
     """
-    return sorted(tweets, key=lambda x: (x.retweets, x.time))
+    return sorted(tweets, key=lambda x: (x.retweets, x.time), reverse=True)
 
 
 def filter_by_hashtag(tweets: list, hashtag: str) -> list:
@@ -98,16 +98,16 @@ if __name__ == '__main__':
     tweet3 = Tweet("@CIA", "We can neither confirm nor deny that this is our first tweet. #heart", 2192, 284200)
     tweets = [tweet1, tweet2, tweet3]
 
-    print(find_fastest_growing(tweets).user)  # -> "@elonmusk"
+    #print(find_fastest_growing(tweets).user)  # -> "@elonmusk"
 
     filtered_by_popularity = sort_by_popularity(tweets)
     print(filtered_by_popularity[0].user)  # -> "@CIA"
     print(filtered_by_popularity[1].user)  # -> "@elonmusk"
     print(filtered_by_popularity[2].user)  # -> "@realDonaldTrump"
 
-    filtered_by_hashtag = filter_by_hashtag(tweets, "#bigsmart")
-    print(filtered_by_hashtag[0].user)  # -> "@realDonaldTrump"
-    print(filtered_by_hashtag[1].user)  # -> "@elonMusk"
+    #filtered_by_hashtag = filter_by_hashtag(tweets, "#bigsmart")
+    #print(filtered_by_hashtag[0].user)  # -> "@realDonaldTrump"
+    #print(filtered_by_hashtag[1].user)  # -> "@elonMusk"
 
     #sorted_hashtags = sort_hashtags_by_popularity(tweets)
     #print(sorted_hashtags[0])  # -> "#heart"
