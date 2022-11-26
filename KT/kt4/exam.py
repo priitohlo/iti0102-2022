b3c680c4e7a3021e9f67db9c45f51ffed4ea65d7
Priit Ohlo <prohlo@ttu.ee>
2022-11-26 15:15:00 +0200

    KT

--- KT/kt4/exam.py
+++ KT/kt4/exam.py
@@ -42,8 +42,8 @@ def sum_elements_around_last_three(nums: list) -> int:
     if 3 in nums and len(nums) >= 3:
         search_nums = list(reversed(nums))
         try:
-            #return search_nums[search_nums.index(3) - 1] + search_nums[search_nums.index(3) + 1]
-            return nums
+            return nums[search_nums.index(3) - 1] + nums[search_nums.index(3) + 1]
+            #return nums
         except IndexError:
             return 0
     else:
@@ -132,12 +132,12 @@ def create_dictionary_from_directed_string_pairs(pairs: list) -> dict:
     return pairs_dict
 
 
-# print(sum_elements_around_last_three([1, 3, 7])) # -> 8
-# print(sum_elements_around_last_three([1, 2, 3, 4, 6, 4, 3, 4, 5, 3, 4, 5, 6])) # -> 9
-# print(sum_elements_around_last_three([1, 2, 3, 4, 6, 4, 3, 4, 5, 3, 3, 2, 3])) # -> 5
-# print(sum_elements_around_last_three([1, 2, 3])) # -> 0
-#
+print(sum_elements_around_last_three([1, 3, 7])) # -> 8
+print(sum_elements_around_last_three([1, 2, 3, 4, 6, 4, 3, 4, 5, 3, 4, 5, 6])) # -> 9
+print(sum_elements_around_last_three([1, 2, 3, 4, 6, 4, 3, 4, 5, 3, 3, 2, 3])) # -> 5
+print(sum_elements_around_last_three([1, 2, 3])) # -> 0
+
 print(sum_elements_around_last_three([1, 3, 4])) # -> 5
 print(sum_elements_around_last_three([3, 2, 1])) # -> 0
-print(sum_elements_around_last_three([3, 2, 1, 3, 2, 0, 3, 2, 1])) # -> 3
+print(sum_elements_around_last_three([3, 2, 1, 3, 2])) # -> 3
 print(sum_elements_around_last_three([4, 5, 3, 2, 3, 6])) # -> 8
