f84851d351a52a598b1be84c53b1789492493811
Priit Ohlo <prohlo@ttu.ee>
2022-11-26 15:47:25 +0200

    KT

--- KT/kt4/exam.py
+++ KT/kt4/exam.py
@@ -41,7 +41,7 @@ def sum_elements_around_last_three(nums: list) -> int:
 
     if 3 in nums and len(nums) >= 3:
         search_nums = list(reversed(nums))
-        if 3 in [search_nums[0], search_nums[-1]] and search_nums[1] != 3:
+        if 3 in [search_nums[0], search_nums[-1]] and search_nums[1] != 3 and len(search_nums) == 3:
             return 0
         try:
             return search_nums[search_nums.index(3) - 1] + search_nums[search_nums.index(3) + 1]
