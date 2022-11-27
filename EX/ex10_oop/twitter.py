c84fd5a4bb89cdfd89b331c4fbc7d88e40b9aa0f
Priit Ohlo <prohlo@ttu.ee>
2022-11-27 10:16:06 +0200

    EX

--- EX/ex10_oop/twitter.py
+++ EX/ex10_oop/twitter.py
@@ -90,24 +90,3 @@ def sort_hashtags_by_popularity(tweets: list) -> list:
     popularities_sorted = dict(sorted(popularities.items(), key=lambda x: x[1], reverse=True))
 
     return list(popularities_sorted.keys())
-
-
-if __name__ == '__main__':
-    tweet1 = Tweet("@realDonaldTrump", "Despite the negative press covfefe #bigsmart", 1249, 54303)
-    tweet2 = Tweet("@elonmusk", "Technically, alcohol is a solution #bigsmart", 366.4, 166500)
-    tweet3 = Tweet("@CIA", "We can neither confirm nor deny that this is our first tweet. #heart", 2192, 284200)
-    tweets = [tweet1, tweet2, tweet3]
-
-    #print(find_fastest_growing(tweets).user)  # -> "@elonmusk"
-
-    #filtered_by_popularity = sort_by_popularity(tweets)
-    #print(filtered_by_popularity[0].user)  # -> "@CIA"
-    #print(filtered_by_popularity[1].user)  # -> "@elonmusk"
-    #print(filtered_by_popularity[2].user)  # -> "@realDonaldTrump"
-
-    #filtered_by_hashtag = filter_by_hashtag(tweets, "#bigsmart")
-    #print(filtered_by_hashtag[0].user)  # -> "@realDonaldTrump"
-    #print(filtered_by_hashtag[1].user)  # -> "@elonMusk"
-
-    sorted_hashtags = sort_hashtags_by_popularity(tweets)
-    print(sorted_hashtags[0])  # -> "#heart"
