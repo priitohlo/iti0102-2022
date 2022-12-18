7a83c43fa75b89a839877d296fa87388e4e731e2
Priit Ohlo <prohlo@ttu.ee>
2022-12-18 20:50:32 +0200

    EX

--- EX/ex12_adventure/adventure.py
+++ EX/ex12_adventure/adventure.py
@@ -63,13 +63,11 @@ class World:
                 for i, c in enumerate(character_list):
                     if c.name == name:
                         self.graveyard.append(character_list.pop(i))
-                        return
 
         if name in [c.name for c in self.graveyard]:
             for i, c in enumerate(character_list):
                 if c.name == name:
                     self.graveyard.remove(i)
-                    return
 
 
 class Adventurer():
