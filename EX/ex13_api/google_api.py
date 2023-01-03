0fdcb31d50e3fd9606f5af699e45679128285ed3
Priit Ohlo <prohlo@ttu.ee>
2023-01-03 10:03:49 +0200

    EX

---
+++ EX/ex13_api/google_api.py
@@ -0,0 +1,46 @@
+from __future__ import print_function
+
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+
+def get_links_from_spreadsheet(id: str, token_file_name: str):
+    creds = None
+    if os.path.exists(token_file_name):
+        creds = Credentials.from_authorized_user_file(token_file_name, SCOPES)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open(token_file_name, 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('sheets', 'v4', credentials=creds)
+
+        sheet = service.spreadsheets()
+        result = sheet.values().get(spreadsheetId=id, range='Songs!A:A').execute()
+        values = result.get('values', [])
+
+        if not values:
+            return 'No data.'
+        else:
+            return values
+
+    except HttpError as err:
+        print(err)
+
+
+if __name__ == '__main__':
+    print(get_links_from_spreadsheet('1WrCzu4p5lFwPljqZ6tMQEJb2vSJQSGjyMsqcYt-yS4M', 'token.json'))
