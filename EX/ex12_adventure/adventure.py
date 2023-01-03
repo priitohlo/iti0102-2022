303b08810cd9ab7c8af15f42f93a7bb5ffe9d36a
Priit Ohlo <prohlo@ttu.ee>
2023-01-03 06:56:59 +0200

    EX

--- EX/ex12_adventure/adventure.py
+++ EX/ex12_adventure/adventure.py
@@ -188,14 +188,14 @@ class World:
                 self.active_monster_list.clear()
             elif self.powers['adventurers'] == self.powers['monsters']:
                 self.calculate_experience(tie=True)
-        elif not deadly and self.powers['adventurers'] != self.powers['monsters']:
+        elif not deadly and self.powers['adventurerss'] != self.powers['monsters']:
             if self.powers['adventurers'] > self.powers['monsters']:
                 self.calculate_experience()
             self.adventurer_list += self.active_adventurer_list
             self.active_adventurer_list.clear()
             self.active_monster_list += self.active_monster_list
             self.active_monster_list.clear()
-        elif not deadly and self.powers['adventurerss'] == self.powers['monsters']:
+        elif not deadly and self.powers['adventurers'] == self.powers['monsters']:
             self.calculate_experience(tie=True)
             self.adventurer_list += self.active_adventurer_list
             self.active_adventurer_list.clear()
