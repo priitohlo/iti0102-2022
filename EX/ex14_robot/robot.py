bcd0da4675f0beefab122289fefdda8b6f6b93ae
Priit Ohlo <prohlo@ttu.ee>
2023-01-09 07:03:03 +0200

    EX

--- EX/ex14_robot/robot.py
+++ EX/ex14_robot/robot.py
@@ -1,5 +1,3 @@
-import time
-
 from FollowerBot import FollowerBot
 
 robot = FollowerBot()
@@ -7,6 +5,6 @@ robot = FollowerBot()
 
 def test_run(robot: FollowerBot):
     robot.set_wheels_speed(100)
-    time.sleep(9)
+    robot.sleep(9)
     robot.set_wheels_speed(0)
     robot.done()
