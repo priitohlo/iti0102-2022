d173968d1bab58d04b42ed1c0bd5c246500f63d8
Priit Ohlo <prohlo@ttu.ee>
2023-01-09 07:25:52 +0200

    EX

--- EX/ex14_robot/robot.py
+++ EX/ex14_robot/robot.py
@@ -1,10 +1,49 @@
 from FollowerBot import FollowerBot
 
-robot = FollowerBot()
-
 
 def test_run(robot: FollowerBot):
+    """
+    Make the robot move, doesnt matter how much, just as long as it has moved from the starting position.
+
+    :param FollowerBot robot: instance of the robot that you need to make move
+    """
     robot.set_wheels_speed(1)
     robot.sleep(9)
     robot.set_wheels_speed(0)
     robot.done()
+
+
+def drive_to_line(robot: FollowerBot):
+    """
+    Drive the robot until it meets a perpendicular black line, then drive forward 25cm.
+
+    There are 100 pixels in a meter.
+
+    :param FollowerBot robot: instance of the robot that you need to make move
+    """
+    robot.set_wheels_speed(20)
+    while robot.get_left_line_sensor() != 0 and robot.get_right_line_sensor() != 0:
+        robot.sleep(1)
+    robot.sleep(3)
+    robot.done()
+
+
+def follow_the_line(robot: FollowerBot):
+    """
+    Create a FollowerBot that will follow a black line until the end of that line.
+
+    The robot's starting position will be just short of the start point of the line.
+
+    :param FollowerBot robot: instance of the robot that you need to make move
+    """
+
+
+def the_true_follower(robot: FollowerBot):
+    """
+    Create a FollowerBot that will follow the black line on the track and make it ignore all possible distractions.
+
+    :param FollowerBot robot: instance of the robot that you need to make move
+    """
+
+
+drive_to_line(FollowerBot(track_image='to_line.png', starting_orientation=90))
