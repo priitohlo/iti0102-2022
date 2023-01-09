f31f71bc0b47826d68f91cb53bf054ad8c6aac10
Priit Ohlo <prohlo@ttu.ee>
2023-01-09 07:03:46 +0200

    EX

--- EX/ex14_robot/robot.py
+++ EX/ex14_robot/robot.py
@@ -4,7 +4,7 @@ robot = FollowerBot()
 
 
 def test_run(robot: FollowerBot):
-    robot.set_wheels_speed(100)
+    robot.set_wheels_speed(1)
     robot.sleep(9)
     robot.set_wheels_speed(0)
     robot.done()
